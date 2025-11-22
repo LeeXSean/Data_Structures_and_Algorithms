5d56a854698de7ac0765337559cc5ec344581473
LeeXSean <1596559262@qq.com>
2025-11-22 20:34:08 +0800

    添加选择排序，插入排序，希尔排序算法

--- 11_基础排序/11_基础排序/src/11_基础排序.cpp
+++ 11_基础排序/11_基础排序/src/11_基础排序.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// 冒泡排序 平均时间复杂度o(n^2)（最好o(n)，最坏o(n^2)） 
+// 空间复杂度o(1)  稳定排序
 void BubbleSort(int* arr, int size)
 {
 	// 外层循环，排除内层循环结束后的最后一个元素（最大元素）
@@ -27,6 +29,78 @@ void BubbleSort(int* arr, int size)
 	}
 }
 
+// 选择排序 平均时间复杂度o(n^2)（最好o(n^2)，最坏o(n^2)） 
+// 空间复杂度o(1) 不稳定排序
+void ChoiceSort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		int k = i;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[j] < arr[k])
+			{
+				k = j;
+			}
+		}
+
+		// 优化，减少不必要的赋值
+		if (k != i)
+		{
+			int tmp = arr[k];
+			arr[k] = arr[i];
+			arr[i] = tmp;
+		}
+	}
+	
+}
+
+// 插入排序 平均时间复杂度o(n^2)（最好o(n)，最坏o(n^2)） 
+// 空间复杂度o(1)  稳定排序 
+// 近似有序下效率最高
+void InsertSort(int* arr, int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		int value = arr[i];
+		int j = i - 1;
+		for (; j >= 0; j--)
+		{
+			if (arr[j] <= value)
+			{
+				break;
+			}
+			arr[j + 1] = arr[j];
+		}
+		arr[j + 1] = value;
+	}
+}
+
+// 希尔排序 依赖不同的增量序列设置平均时间复杂度o(n^1.3)（最好o(n)，最坏o(n^2)） 
+// 空间复杂度o(1)  不稳定排序
+// 插入排序的优化版，分组进行插入排序，逐步将全局元素趋向于有序，最后全局采用插入排序
+void ShellSort(int* arr, int size)
+{
+	for (int gap = size / 2; gap > 0; gap /= 2) // 基本不影响性能
+	{
+		// 交叉对同余类的元素进行插入排序
+		for (int i = gap; i < size; i++) // o(n)
+		{
+			int value = arr[i];
+			int j = i - gap;
+			for (; j >= 0; j -= gap) // o(n)
+			{
+				if (arr[j] <= value)
+				{
+					break;
+				}
+				arr[j + gap] = arr[j];
+			}
+			arr[j + gap] = value;
+		}
+	}
+}
+
 int main()
 {
 	int arr[10];
@@ -44,7 +118,13 @@ int main()
 	}
 	std::cout << std::endl;
 
+	/*
 	BubbleSort(arr, size);
+	ChoiceSort(arr, size);
+	InsertSort(arr, size);
+	*/
+
+	ShellSort(arr, size);
 	
 	for (int v : arr)
 	{
