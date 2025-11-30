1773e9dcf71bfff2aecff9ee39538f174c883786
LeeXSean <1596559262@qq.com>
2025-11-30 16:09:13 +0800

    优化快速排序算法

--- 12_快速排序/12_快速排序/src/12_快速排序.cpp
+++ 12_快速排序/12_快速排序/src/12_快速排序.cpp
@@ -1,8 +1,72 @@
+/*
+ * 快速排序算法
+ *
+ * 时间复杂度：
+ *   - 平均：O(n log n)
+ *   - 最好：O(n log n)
+ *   - 最坏：O(n^2)
+ *     [原因]：当数据原本有序（或接近有序）且基准值选择不当时，递归树退化为链表，深度为 n。
+ *
+ * 空间复杂度：
+ *   - O(log n) ~ O(n)
+ *     [原因]：主要消耗在于递归调用的栈空间。
+ *     - 平均情况递归深度为 log n。
+ *     - 最坏情况递归深度为 n。
+ */
+
+
 #include <iostream>
 
-// 快排分割函数
-int Partation(int* arr, int l, int r)
+// 针对区间的插入排序算法
+void InsertSort(int* arr, int begin, int end)
 {
+	for (int i = begin + 1; i <= end; i++)
+	{
+		int value = arr[i];
+		int j = i - 1;
+		for (; j >= begin; j--)
+		{
+			if (arr[j] <= value)
+			{
+				break;
+			}
+			arr[j + 1] = arr[j];
+		}
+		arr[j + 1] = value;
+	}
+}
+
+// 快排分割函数
+int Partition(int* arr, int l, int r)
+{
+	// 优化二：基准数采取三数取中法
+	int mid = (l + r) / 2;
+	
+	if (arr[l] > arr[mid])
+	{
+		int tmp = arr[mid];
+		arr[mid] = arr[l];
+		arr[l] = tmp;
+	}
+
+	if (arr[mid] > arr[r])
+	{
+		int tmp = arr[r];
+		arr[r] = arr[mid];
+		arr[mid] = tmp;
+	}
+
+	if (arr[l] > arr[mid])
+	{
+		int tmp = arr[mid];
+		arr[mid] = arr[l];
+		arr[l] = tmp;
+	}
+
+	int tmp = arr[mid];
+	arr[mid] = arr[l];
+	arr[l] = tmp;
+
 	// 记录基准数
 	int val = arr[l];
 
@@ -20,7 +84,7 @@ int Partation(int* arr, int l, int r)
 			l++;
 		}
 		
-		while (l < r && arr[l] < val)
+		while (l < r && arr[l] <= val)
 		{
 			l++;
 		}
@@ -44,8 +108,12 @@ void QuickSort(int* arr, int begin, int end)
 	if (begin >= end)
 		return;
 
+	// 优化一：小范围下数据趋于有序，采用插入排序（具体范围依赖测试数据集）
+	if (end - begin < 5)
+		return InsertSort(arr, begin, end);
+
 	// 在[begin, end]区间做一次快排分割处理
-	int pos = Partation(arr, begin, end);
+	int pos = Partition(arr, begin, end);
 
 	// 对基准数左边和右边的序列分别做快排（递归）
 	QuickSort(arr, begin, pos - 1);
